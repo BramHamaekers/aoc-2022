9d97061c1f855ba342254c9c2ab77a1b520ce6f5
BramHamaekers <bram.hamaekers@gmail.com>
2022-12-04 17:18:05 +0100

    day_1 simplification using vectors

--- day_1/aoc_2022_1.cpp
+++ day_1/aoc_2022_1.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     ifstream fileStream;
     fileStream.open("input.txt");
-    int total = 0;
-    list<int> total_list;
 
+    int total = 0;
+    vector<int> total_v;
     string line;
     while(getline(fileStream, line)) {
         if (line.empty()) {
-            total_list.push_back(total);
+            total_v.push_back(total);
             total = 0;
             continue;
         }
         total = total + stoi(line);
     }
-    total_list.push_back(total);
-    total_list.sort();
-    total_list.reverse();
-
-    int top = total_list.front();
-    int top3 = 0;
-    for(int i = 0; i < 3; i++) {
-        top3 = top3 + total_list.front();
-        total_list.pop_front();
-    }
-    cout << "Part 1: " << top << endl;
-    cout << "Part 2: " << top3 << endl;
+    total_v.push_back(total);
+    sort(total_v.begin(), total_v.end());
+    cout << "Part 1: " << total_v.back() << endl;
+    cout << "Part 2: " << total_v.back() + total_v[total_v.size()-2] + total_v[total_v.size()-3] << endl;
     return 0;
 }
