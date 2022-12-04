114a28b6b03161412a9238f52e72eada7873bb88
BramHamaekers <bram.hamaekers@gmail.com>
2022-12-04 12:42:39 +0100

    day 4 revision

--- day_4/aoc_2022_4.cpp
+++ day_4/aoc_2022_4.cpp
@@ -1,64 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-
+#include <tuple>
 
 using namespace std;
 
-bool contains(string range_1, string range_2) {
-    int dash_1 = range_1.find('-');
-    int dash_2 = range_2.find('-');
-    int min_1 = stoi(range_1.substr(0,dash_1));
-    int max_1 = stoi(range_1.substr(dash_1 + 1));
-    int min_2 = stoi(range_2.substr(0,dash_2));
-    int max_2 = stoi(range_2.substr(dash_2 + 1));
+tuple<int, int, int, int> get_boundries(string line) {
+    int comma = line.find(',');
+    string range_1 = line.substr(0, comma), range_2 = line.substr(comma+1);
+    int dash_1 = range_1.find('-'), dash_2 = range_2.find('-');
+    return make_tuple (
+        stoi(range_1.substr(0, dash_1)),
+        stoi(range_1.substr(dash_1 + 1)),
+        stoi(range_2.substr(0, dash_2)),
+        stoi(range_2.substr(dash_2 + 1)));
+}
+
+bool contains(int min_1, int max_1, int min_2, int max_2) {
     return (min_1 <= min_2 && max_1 >= max_2) || (min_1 >= min_2 && max_1 <= max_2);
 }
 
-bool overlaps(string range_1, string range_2) {
-    int dash_1 = range_1.find('-');
-    int dash_2 = range_2.find('-');
-    int min_1 = stoi(range_1.substr(0,dash_1));
-    int max_1 = stoi(range_1.substr(dash_1 + 1));
-    int min_2 = stoi(range_2.substr(0,dash_2));
-    int max_2 = stoi(range_2.substr(dash_2 + 1));
-    return (min_1 <= min_2 && min_2 <= max_1) || (min_1 <= max_2 && max_2 <= max_1) || contains(range_1, range_2);
-}
-
-int part_1() {
-    ifstream fileStream;
-    fileStream.open("input.txt");
-
-    int comma;
-    int total = 0;
-    string line;
-    while (getline(fileStream, line)) {
-        comma = line.find(',');
-        string range_1 = line.substr(0, comma);
-        string range_2 = line.substr(comma+1);
-        total += contains(range_1, range_2);
-    }
-    return total;
-}
-
-int part_2() {
-    ifstream fileStream;
-    fileStream.open("input.txt");
-
-    int comma;
-    int total = 0;
-    string line;
-    while (getline(fileStream, line)) {
-        comma = line.find(',');
-        string range_1 = line.substr(0, comma);
-        string range_2 = line.substr(comma+1);
-        total += overlaps(range_1, range_2);
-    }
-    return total;
+bool overlaps(int min_1, int max_1, int min_2, int max_2) {
+    return (min_1 <= min_2 && min_2 <= max_1) || (min_1 <= max_2 && max_2 <= max_1) || contains(min_1, max_1, min_2, max_2);
 }
 
 int main() {
-    cout << "Part 1: " << part_1() << endl;
-    cout << "Part 2: " << part_2() << endl;
+    ifstream fileStream;
+    fileStream.open("input.txt");
+
+    int contains_total = 0, overlaps_total = 0;
+    int min_1, max_1, min_2, max_2;
+    tuple<int, int, int, int> boundries;
+    string line;
+    while (getline(fileStream, line)) {
+        tie(min_1, max_1, min_2, max_2) = get_boundries(line);
+        contains_total += contains(min_1, max_1, min_2, max_2);
+        overlaps_total += overlaps(min_1, max_1, min_2, max_2);
+    }
+    cout << "Part 1: " << contains_total << endl;
+    cout << "Part 2: " << overlaps_total << endl;
+    return 0;
 }
